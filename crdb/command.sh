5759ec2138d91605f936180e149c15fd492fbe15
Bucur Alexandru Dragos <dragos.bucur1105@gmail.com>
2024-08-25 23:29:41 +0300

    unused script

--- crdb/command.sh
+++
@@ -1,2 +0,0 @@
-./runner.sh -d single -t Documents -m DB -s 500K -h Okapi -b
-
--- crdb/scripts/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-cockroach sql --insecure -f ./init.sql
-
-echo "Created tables"
-
-cockroach sql --insecure -f ./import_data_500k.sql
